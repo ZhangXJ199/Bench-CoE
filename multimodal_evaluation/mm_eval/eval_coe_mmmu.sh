d07f02df55026d1153cbb5e3cf074c59ebea4ff5
ZhangXJ199 <1589363406@qq.com>
2024-12-08 14:36:08 +0800

    24.12.8

--- multimodal_evaluation/mm_eval/eval_coe_mmmu.sh
+++ multimodal_evaluation/mm_eval/eval_coe_mmmu.sh
@@ -1,15 +1,17 @@
+DATASET_DIR="/data/vlm/llava_data/eval/MMMU"
+
 python -m eval_coe_mmmu \
-    --model1_path /mnt/data/zxj/checkpoints/TinyLLaVA-Phi-2-SigLIP-3.1B \
-    --model2_path /mnt/data/zxj/checkpoints/Bunny-v1_1-4B \
-    --bert_model_path /mnt/data/zxj/checkpoints/trained_model/bert_mmmu \
-    --image-folder /mnt/data/zxj/data/MMMU/all_images \
-    --question-file /mnt/data/zxj/data/MMMU/anns_for_eval.json \
-    --answers-file /root/coe/zxj/mmmu_eval/answers/coe.jsonl \
+    --model1_path /data/vlm/zxj/checkpoints/TinyLLaVA-Phi-2-SigLIP-3.1B \
+    --model2_path /data/vlm/zxj/checkpoints/Bunny-v1_1-4B \
+    --bert_model_path /data/vlm/zxj/checkpoints/subject_bert_mmmu \
+    --image-folder $DATASET_DIR/all_images \
+    --question-file $DATASET_DIR/anns_for_eval.json \
+    --answers-file $DATASET_DIR/answers/coe.jsonl \
 
 python convert_answer_to_mmmu.py \
-    --answers-file /root/coe/zxj/mmmu_eval/answers/coe.jsonl \
-    --answers-output /root/coe/zxj/mmmu_eval/answers/coe.json
+    --answers-file $DATASET_DIR/answers/coe.jsonl \
+    --answers-output $DATASET_DIR/answers/coe.json
 
-cd /mnt/data/zxj/data/MMMU/eval
+cd $DATASET_DIR/eval
 
-python main_eval_only.py --output_path /root/coe/zxj/mmmu_eval/answers/coe.json
+python main_eval_only.py --output_path $DATASET_DIR/answers/coe.json
